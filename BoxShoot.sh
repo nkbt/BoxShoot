41bb6e9a77c1b9c5e9b4c7b541054123e501ab9c
Nikita Butenko <quall.gm@gmail.com>
2013-02-02 00:26:16 +1100

    Removed unnecessary file

--- BoxShoot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-F_NAME=`date +%s | md5sum | awk '{ print $1}'`'.png'
-DBOX_DIR='/home/username/Dropbox/Public/pic/'
-DBOX_URL='http://dl.dropbox.com/u/2058666/pic/'
-S_NAME=$DBOX_DIR$F_NAME
-
-scrot -s -q 0 $S_NAME
-echo -n $DBOX_URL$F_NAME | xclip
